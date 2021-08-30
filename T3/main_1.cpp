6155f2ed9d53be217b82502a6d0e285e6cae7406
Aymeric CLAUZEL <Aymeric.CLAUZEL@cpnv.ch>
2021-08-30 22:22:25 +0200

    euh je sais pas trop

--- T3/main_1.cpp
+++ T3/main_1.cpp
@@ -14,86 +14,81 @@
 
 using namespace std;
 
-class game
-{
-    const int Perso = 0;
-    const int screenWidth{ 800 };
-    const int screenHeight{ 450 };
-    Texture2D Perso = LoadTexture("img\perso0.png");
-    // const string nomJeu{"Nem du jeu"};
-    const char* nomJeu{ "Nom du jeu" };
-    Vector2 ballPosition = { (float)screenWidth / 2, (float)screenHeight / 2 };
 
-public:
-    game() {
+  
 
-
-        InitWindow(screenWidth, screenHeight, nomJeu);
-
-
-
-        SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-        //--------------------------------------------------------------------------------------
-
-        // Main game loop
-        while (!WindowShouldClose())    // Detect window close button or ESC key
-        {
-            update();
-
-            BeginDrawing();
-            draw();
-            EndDrawing();
-
-        }
-
-        // De-Initialization
-        //--------------------------------------------------------------------------------------
-        CloseWindow();        // Close window and OpenGL context
-        //--------------------------------------------------------------------------------------
-
-    }
-
-
-    void update()
-    {
-
-        if (IsKeyDown(KEY_RIGHT))
-            ballPosition.x += 2.0;
-        if (IsKeyDown(KEY_LEFT))
-            ballPosition.x -= 2.0;
-        if (IsKeyDown(KEY_UP))
-            ballPosition.y -= 2.0;
-        if (IsKeyDown(KEY_DOWN))
-            ballPosition.y += 2.0;
-
-    }
-
-    void draw()
-    {
-
-
-        ClearBackground(RAYWHITE);
-
-        DrawText("move the ball with arrow keys", 10, 10, 20, DARKGRAY);
-
-        DrawCircleV(ballPosition, 50, MAROON);
-
-
-    }
-
-
-
-};
 
 
 
 
 int main(void)
 {
+   
+    const int screenWidth{ 800 };
+    const int screenHeight{ 450 };
+    
+    void draw( Texture2D,Vector2);
+    void update(Vector2);
+    // const string nomJeu{"Nem du jeu"};
+    const char* nomJeu{ "Nom du jeu" };
+    Vector2 ballPosition = { (float)screenWidth / 2, (float)screenHeight / 2 };
 
-    game game1;
+    
 
 
+    InitWindow(screenWidth, screenHeight, nomJeu);
+    Texture2D Perso = LoadTexture("img/perso0.png");
+
+
+    SetTargetFPS(25);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        update(ballPosition);
+
+        BeginDrawing();
+        DrawTexture(Perso, ballPosition.x,ballPosition.y,Color(BLACK));
+        draw(Perso,ballPosition);
+        EndDrawing();
+
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+  
 
     return 0;
 }
+void update(Vector2 ballPosition_)
+{
+
+    if (IsKeyDown(KEY_RIGHT))
+        ballPosition_.x += 2.0;
+    if (IsKeyDown(KEY_LEFT))
+        ballPosition_.x -= 2.0;
+    if (IsKeyDown(KEY_UP))
+        ballPosition_.y -= 2.0;
+    if (IsKeyDown(KEY_DOWN))
+        ballPosition_.y += 2.0;
+
+}
+
+void draw(Texture2D Perso_, Vector2 ballPosition_)
+{
+
+
+    ClearBackground(RAYWHITE);
+
+    DrawText("move the ball with arrow keys", 10, 10, 20, DARKGRAY);
+
+    DrawTextureEx(Perso_, ballPosition_, 0.0, 100, Color(BLACK));
+
+
+
+
+};
